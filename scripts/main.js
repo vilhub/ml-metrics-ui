b8aa2a57b5dfae034bc9cb97eb37582de50da220
Vil <cardboardburger@gmail.com>
2023-01-26 19:30:53 +0100

    Add computation of f1 score

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,33 @@
 // calculate f1 score 
 function calculateF1Score(predictions, groundTruth) {
-    // your code for calculating f1 score
-    return 1.0;
+    let truePositives = {'A': 0, 'B': 0, 'C': 0};
+    let falsePositives = {'A': 0, 'B': 0, 'C': 0};
+    let falseNegatives = {'A': 0, 'B': 0, 'C': 0};
+    let classes = ['A','B','C'];
+
+    for (let i = 0; i < predictions.length; i++) {
+        if (predictions[i] === groundTruth[i]) {
+            truePositives[predictions[i]]++;
+        } else {
+            falsePositives[predictions[i]]++;
+            falseNegatives[groundTruth[i]]++;
+        }
+    }
+
+    let precision = 0;
+    let recall = 0;
+    let f1Score = 0;
+    for(let cl of classes){
+        precision += truePositives[cl] / (truePositives[cl] + falsePositives[cl]);
+        recall += truePositives[cl] / (truePositives[cl] + falseNegatives[cl]);
+    }
+
+    precision /= classes.length;
+    recall /= classes.length;
+
+    f1Score = 2 * ((precision * recall) / (precision + recall));
+
+    return f1Score;
 }
 
 let predictions = [];
