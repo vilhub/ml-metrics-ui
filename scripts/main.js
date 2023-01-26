c0ce16f753b5592f9ae5c173a2107980bd11fc11
Vil <cardboardburger@gmail.com>
2023-01-26 19:16:47 +0100

    Change to list of elements

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,27 @@ function calculateF1Score(predictions, groundTruth) {
     return 1.0;
 }
 
-// update score on page
-function updateScore() {
-    let predictions = ["A", "B", "C", "A", "B"];
-    let groundTruth = ["A", "A", "C", "A", "B"];
-    let score = calculateF1Score(predictions, groundTruth);
-    document.getElementById("score").innerHTML = score;
-}
-
+let predictions = [];
+let groundTruth = [];
 let predictionsSelect = document.getElementById("predictionsSelect");
 let groundTruthSelect = document.getElementById("groundTruthSelect");
 let selectedPredictions = document.getElementById("selectedPredictions");
 let selectedGroundTruth = document.getElementById("selectedGroundTruth");
 let score = document.getElementById("score");
 
-predictionsSelect.addEventListener("change", function() {
-  selectedPredictions.value = predictionsSelect.value;
-  updateScore();
-});
 
-groundTruthSelect.addEventListener("change", function() {
-  selectedGroundTruth.value = groundTruthSelect.value;
-  updateScore();
+function updateScore() {
+    let score = calculateF1Score(predictions, groundTruth);
+    document.getElementById("score").value = score;
+}
+
+document.getElementById("predictionsSelect").addEventListener("change", function () {
+    predictions.push(this.value);
+    selectedPredictions.textContent = predictions;
+    updateScore();
+});
+document.getElementById("groundTruthSelect").addEventListener("change", function () {
+    groundTruth.push(this.value);
+    selectedGroundTruth.textContent = groundTruth;
+    updateScore();
 });
